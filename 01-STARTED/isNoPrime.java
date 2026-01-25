ec6e94ff2d5d451b97a649f17c5f423b2897d35d
Amishrax18 <arjunmishravk18@gmail.com>
2026-01-25 17:39:30 +0530

    add

--- 01-STARTED/isNoPrime.java
+++ 01-STARTED/isNoPrime.java
@@ -1,5 +1,6 @@
+
 import java.util.*;
-public class primeNo{
+public class isNoPrime{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
