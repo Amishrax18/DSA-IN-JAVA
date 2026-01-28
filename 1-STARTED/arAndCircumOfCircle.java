7bf00b5e1b679b30bbc1f6b2de17f2be4245b443
Amishrax18 <arjunmishravk18@gmail.com>
2026-01-28 19:19:39 +0530

    Create arAndCircumOfCircle.java

---
+++ 1-STARTED/arAndCircumOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class arAndCircumOfCircle  {
+public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    float radius = sc.nextFloat();
+    float Area=3.14f*radius*radius;
+    float Circumference = 2*3.14f*radius;
+    System.out.println(Area);
+    System.out.println(Circumference);
+}
+} 
