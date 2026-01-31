9b5f34dc6a136ee0281892cde454c642d82bdfd8
Amishrax18 <arjunmishravk18@gmail.com>
2026-01-31 19:06:57 +0530

    Update printSquarePattern.java

--- 1-STARTED/printSquarePattern.java
+++ 1-STARTED/printSquarePattern.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class printSquarePattern{
+public class printsquarePattern{
     public static void main(String args[]){
         for(int line=1 ; line<=4; line++){
              System.out.println("****");
