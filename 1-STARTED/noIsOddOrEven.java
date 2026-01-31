7c2a144272bf5265b020b69428c704e616370fea
Amishrax18 <arjunmishravk18@gmail.com>
2026-01-31 19:05:19 +0530

    Update noIsOddOrEven.java

--- 1-STARTED/noIsOddOrEven.java
+++ 1-STARTED/noIsOddOrEven.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class noIsOddOrEven {
+public class noIsoddOrEven {
 public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
     int number = sc.nextInt();
