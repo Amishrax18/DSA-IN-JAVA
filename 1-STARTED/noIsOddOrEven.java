36b1892778baff0a08d421dd4ccd11d72bfb81da
Amishrax18 <arjunmishravk18@gmail.com>
2026-01-31 19:04:15 +0530

    Update noIsOddOrEven.java

--- 1-STARTED/noIsOddOrEven.java
+++ 1-STARTED/noIsOddOrEven.java
@@ -4,7 +4,7 @@ public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
     int number = sc.nextInt();
     if(number % 2==0){
-      System.out.println("Entered number is odd");
+      System.out.println("Entered number is even");
         }
         else{
          System.out.println("Entered number is odd");
