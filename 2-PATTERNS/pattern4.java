a1431afe37525e4c4a603631e3c5c2e33e117839
Amishrax18 <arjunmishravk18@gmail.com>
2026-02-02 19:31:36 +0530

    Create pattern4.java

---
+++ 2-PATTERNS/pattern4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class pattern4 {
+public static void main(String args[]){
+    char ch = 'A';
+    int n = 4;
+    for(int line=1 ; line<=n ; line++){
+        for(int chars=1 ; chars<=line ; chars++){
+              System.out.print(ch);
+              ch++;
+        }
+          System.out.println();
+    }
+}
+}
