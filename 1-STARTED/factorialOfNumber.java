3f02a1f65c947547f2fe02527a5a2d65a152970c
Amishrax18 <arjunmishravk18@gmail.com>
2026-02-03 19:11:42 +0530

    Create factorialOfNumber.java

---
+++ 1-STARTED/factorialOfNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class factorialOfNumber {
+public static int factorial(int n){
+    int fact=1;
+    for(int i=1 ; i<=n ; i++){
+        fact=fact*i;
+    }
+    return fact;
+}
+public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int n=sc.nextInt();
+    int fact=factorial(n);
+    System.out.println(fact);
+}
+}
