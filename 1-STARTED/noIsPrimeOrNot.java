7001b64a8dd00c81516577b8689289ccc26d9723
Amishrax18 <arjunmishravk18@gmail.com>
2026-02-04 18:51:03 +0530

    Update noIsPrimeOrNot.java

--- 1-STARTED/noIsPrimeOrNot.java
+++ 1-STARTED/noIsPrimeOrNot.java
@@ -3,7 +3,7 @@ public class noIsPrimeOrNot{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
-        if(n==1){
+        if(n==2){
             System.out.println("no is prime");
         }
         for(int i=2;i<=n/2;i++){
