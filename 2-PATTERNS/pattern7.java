d06961cc2a8cae927deadba020a450de02113ec8
Amishrax18 <arjunmishravk18@gmail.com>
2026-02-05 19:19:11 +0530

    Create pattern7.java

---
+++ 2-PATTERNS/pattern7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class pattern7{
+    public static void inverted_halfpyramid(int rows){
+        for (int i=1 ; i<=rows ; i++){
+        for (int j=1 ; j<=rows-i+1 ; j++){
+            System.out.print(j);
+        }
+        System.out.println();
+        }
+      
+    }
+    
+public static void main(String args[]){
+  inverted_halfpyramid(5);
+    }
+}
+    
