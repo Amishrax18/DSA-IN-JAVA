7a336c0dbc020817f0111af4265ffcb80f59b977
Amishrax18 <arjunmishravk18@gmail.com>
2026-02-09 19:57:05 +0530

    Create reverseArray.java

---
+++ 3-ARRAYS/reverseArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class reverseArray{
+    public static void reverse(int numbers[]){
+        int first=0;
+        int last=numbers.length-1;
+        while(first<last){
+            int temp=numbers[last];
+            numbers[last]=numbers[first];
+            numbers[first]=temp;
+            first++;
+            last--;
+
+        }
+        }
+        public static void main(String args[]){
+        int numbers[]={3,5,2,6,7,1};
+        reverse(numbers);
+        for(int i=0;i<numbers.length;i++){
+            System.out.print(numbers[i]+" ");
+            }
+        }
+}
