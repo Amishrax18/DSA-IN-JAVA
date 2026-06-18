af27de0672d21e49172c6771d46516b7937117cd
Amishrax18 <arjunmishravk18@gmail.com>
2026-06-18 12:39:44 +0530

    as

--- FUNCTION/primeNumber.java
+++ FUNCTION/primeNumber.java
@@ -4,6 +4,7 @@ public class primeNumber {
         for(int i=2;i*i<=n;i++){
             if(n%i==0){
                 System.out.println("number is not prime");
+                break;
             }
             else{
                 System.out.println("number is not prime");
@@ -11,7 +12,7 @@ public class primeNumber {
         }
     }
     public static void main(String[] args) {
-        
+        isPrimeOrNot(234);
     }
     
 }
